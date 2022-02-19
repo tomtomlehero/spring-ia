2753b22ea478179bec5e52d8c5e262d6ce003479
Mathieu <mathieu@live.co.uk>
2022-02-19 17:09:12 +0100

    Home page

---
+++ src/main/java/fr/mla/springia/controller/HomeController.java
@@ -0,0 +1,13 @@
+package fr.mla.springia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+}
