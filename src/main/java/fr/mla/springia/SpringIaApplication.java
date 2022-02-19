04da78097690a479489016698394895cdaca4e12
Mathieu <mathieu@live.co.uk>
2022-02-19 15:26:16 +0100

    Initial Commit

---
+++ src/main/java/fr/mla/springia/SpringIaApplication.java
@@ -0,0 +1,13 @@
+package fr.mla.springia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/mla/springia/SpringIaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mla.springia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
